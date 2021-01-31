a8557c1d662afc72578a65ff32ff24f5fa7e73fd
Adrian Shedley <adrian.shedley@gmail.com>
2021-01-31 16:22:15 +0800

    win key

--- Assets/Scripts/grabbableScript.cs
+++ Assets/Scripts/grabbableScript.cs
@@ -7,29 +7,30 @@ public class grabbableScript : MonoBehaviour
     public bool grabbed;
     RaycastHit2D hit;
     public float distance = 2f;
+    public GameObject winMenuScript;
     public Transform holdpoint;
     public float throwforce;
     public LayerMask notgrabbed;
 
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.B))
+        if (Input.GetKeyDown(KeyCode.B))
         {
-            if(!grabbed)
+            if (!grabbed)
             {//grab
-                Physics2D.queriesStartInColliders=false;
+                Physics2D.queriesStartInColliders = false;
 
                 Vector2 dir = new Vector2(1, 0);
                 Vector3 angle = transform.rotation.eulerAngles;
 
                 print("euler angles " + angle);
 
-                if (Mathf.RoundToInt(angle.z) == 0) { dir = new Vector2(1, 0);  }
+                if (Mathf.RoundToInt(angle.z) == 0) { dir = new Vector2(1, 0); }
                 else if (Mathf.RoundToInt(angle.z) == -90) { dir = new Vector2(0, -1); }
                 else if (Mathf.RoundToInt(angle.z) == -180) { dir = new Vector2(-1, 0); }
                 else if (Mathf.RoundToInt(angle.z) == -270) { dir = new Vector2(0, 1); }
@@ -37,37 +38,45 @@ public class grabbableScript : MonoBehaviour
                 Vector2 back = -distance * 0.5f * dir;
                 Vector3 backOffset = new Vector3(back.x, back.y, 0);
 
-                hit = Physics2D.Raycast(transform.position, dir*transform.localScale.x, distance);
-                
-                if(hit.collider!=null && hit.collider.tag=="grabbable")
+                hit = Physics2D.Raycast(transform.position, dir * transform.localScale.x, distance);
+
+                if (hit.collider != null && hit.collider.tag == "grabbable")
                 {
-                    grabbed=true;
+                    grabbed = true;
                     Debug.Log("object grabbed");
                 }
-            }
-            else if(!Physics2D.OverlapPoint(holdpoint.position, notgrabbed)) //stops you from dropping objects in the terrain/garbbing the terrain
-            {//throw
-                grabbed=false;
-                Debug.Log("object released");
-                if(hit.collider.gameObject.GetComponent<Rigidbody2D>()!=null)
-                {//angle of throw. faces same direction as play + up.
-                    hit.collider.gameObject.GetComponent<Rigidbody2D>().velocity=new Vector2(transform.localScale.x,1)*throwforce;
-                    
+
+                if (hit.collider != null && hit.collider.tag == "Key")
+                {
+                    // you win
+                    print("you win");
+                    winMenuScript.GetComponent<WinMenu>().GotTheKey();
                 }
+                else if (!Physics2D.OverlapPoint(holdpoint.position, notgrabbed)) //stops you from dropping objects in the terrain/garbbing the terrain
+                {//throw
+                    grabbed = false;
+                    Debug.Log("object released");
+                    if (hit.collider.gameObject.GetComponent<Rigidbody2D>() != null)
+                    {//angle of throw. faces same direction as play + up.
+                        hit.collider.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(transform.localScale.x, 1) * throwforce;
+
+                    }
+                }
+
+            }
+
+            if (grabbed)
+            {
+                hit.collider.gameObject.transform.position = holdpoint.position; //whatever we grab is held in this poisiton
             }
 
         }
 
-        if(grabbed)
+        void OnDrawGizmos()
         {
-            hit.collider.gameObject.transform.position=holdpoint.position; //whatever we grab is held in this poisiton
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(transform.position, transform.position + Vector3.right * transform.localScale.x * distance);
         }
-
-    }
-
-    void OnDrawGizmos()
-    {
-        Gizmos.color = Color.green;
-        Gizmos.DrawLine(transform.position,transform.position+Vector3.right*transform.localScale.x*distance);
     }
 }
+
